397645c42bb80de8780963239d5842ad2ac57abe
praneeth323 <46974375+praneeth323@users.noreply.github.com>
2019-10-23 12:46:46 -0500

    updated 110.

--- data.js
+++ data.js
@@ -17,7 +17,7 @@ module.exports = [
   { "id": 107},
   { "id": 108},
   { "id": 109},
-  { "id": 110},
+  { "id": 110, "fact": "JavaScript is supported by all modern Web browsers with the built-in interpreters.", "by": "https://github.com/praneeth323"},
   { "id": 111},
   { "id": 112},
   { "id": 113},
